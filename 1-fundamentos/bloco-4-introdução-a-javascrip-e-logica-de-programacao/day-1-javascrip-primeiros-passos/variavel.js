ae817482e27de4a585171000f17b5e9eb6126560
Pablo Dominguez <pasaxo@gmail.com>
2022-08-18 11:32:28 -0300

    Variavies

---
+++ 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-1-javascrip-primeiros-passos/variavel.js
@@ -0,0 +1,14 @@
+const myName = 'Pablo';
+const birthCity = 'Rio de Janeiro';
+let birthYear = 1976;
+
+birthYear = 2030;
+
+/*birthCity = 'Angra dos Reis'*/
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+/*O erro ocorre porque queremos alterar uma constate como se fosse uma variavel*/
+
