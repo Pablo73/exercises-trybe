ab995ac44c0d6eb4136cc8001b6404f1e5d4f596
Pablo Dominguez <pasaxo@gmail.com>
2022-08-18 14:21:13 -0300

    Exercicio primitivo tipagem aritmetica

---
+++ 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-1-javascrip-primeiros-passos/tipo-primitivo-tipagem-dinamica-operacao-aritmetica.js
@@ -0,0 +1,20 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+//console.log(typeof patientAge);
+console.log(typeof patientId);
+
+patientId = '50';
+
+const base = 5;
+let height = 8;
+const area = base * height;
+console.log(area);
+const perimeter = 2 * base + 2 * height;
+console.log(perimeter);
+
