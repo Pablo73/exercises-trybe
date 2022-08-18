cf9a78d5d57864d3c8a7f9133c5e9f217b53299c
Pablo Dominguez <pasaxo@gmail.com>
2022-08-18 15:01:22 -0300

    Exercicio IF ELSE

---
+++ 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-1-javascrip-primeiros-passos/Condicaoes-if-e-else.js
@@ -0,0 +1,15 @@
+let notaCandidato = 100;
+
+if( notaCandidato > 100){
+    console.log('Você fez trapaça');
+}
+else if(notaCandidato >= 80 && notaCandidato <=100){
+console.log('Parabéns, você foi aprovada(o)!');
+} 
+else if(notaCandidato < 80 && notaCandidato >= 60){
+    console.log('Você está na nossa lista de espera');
+} 
+else { console.log('Você foi reprovado(a)');
+}
+
+
