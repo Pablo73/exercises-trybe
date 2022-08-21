35192678a354bbafebecc2fd72944088854bdaeb
Pablo Dominguez <pasaxo@gmail.com>
2022-08-21 11:15:51 -0300

    exercicio 1 e fixação

---
+++ 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-3-javascrip-logica-de-programacao-e-algoritmo/1-fixando.js
@@ -0,0 +1,15 @@
+const n = 9 ;
+
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);
+
+// let resultado = 0;
+// for(let i = 0 ; i <= 100 ; i = i + 1){
+// resultado = resultado + i
+// }
+// console.log(resultado);
+
+
---
+++ 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-3-javascrip-logica-de-programacao-e-algoritmo/2-exercicio.js
@@ -0,0 +1,11 @@
+// Exrcicio 1 - crie um algoritmo que retorne o fatorial de 10.
+
+let fatorial = 10;
+let resultado = 1;
+for(let i = 1 ; i <= fatorial ; i = i + 1){
+resultado = resultado * i;
+}
+console.log(resultado);
+
+
+
