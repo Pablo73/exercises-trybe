8b51420c88c31de765f9eeae9e9d58b242c7bcf5
Pablo Dominguez <pasaxo@gmail.com>
2022-08-21 12:15:06 -0300

    exercicio 2

--- 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-3-javascrip-logica-de-programacao-e-algoritmo/2-exercicio.js
+++ 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-3-javascrip-logica-de-programacao-e-algoritmo/2-exercicio.js
@@ -1,11 +1,21 @@
 // Exrcicio 1 - crie um algoritmo que retorne o fatorial de 10.
 
-let fatorial = 10;
-let resultado = 1;
-for(let i = 1 ; i <= fatorial ; i = i + 1){
-resultado = resultado * i;
-}
-console.log(resultado);
+// let fatorial = 10;
+// let resultado = 1;
+// for(let i = 1 ; i <= fatorial ; i = i + 1){
+// resultado = resultado * i;
+// }
+// console.log(resultado);
 
+
+// Exrcicio 2 -  Agora, desenvolva um algoritmo que é capaz de inverter uma palavra.
+
+let word = 'tryber';
+let resultado = '';
+for(let i = word.length ; i >= 0 ; i = i - 1){
+resultado = resultado + word[i];
+}
+
+console.log(resultado);
 
 
