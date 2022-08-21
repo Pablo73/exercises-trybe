1baef173c97e62a5027ed8e9b5b409609c5c4974
Pablo Dominguez <pasaxo@gmail.com>
2022-08-21 16:09:04 -0300

    atualização

--- 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-3-javascrip-logica-de-programacao-e-algoritmo/2-exercicio.js
+++ 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-3-javascrip-logica-de-programacao-e-algoritmo/2-exercicio.js
@@ -10,12 +10,27 @@
 
 // Exrcicio 2 -  Agora, desenvolva um algoritmo que é capaz de inverter uma palavra.
 
-let word = 'tryber';
-let resultado = '';
-for(let i = word.length ; i >= 0 ; i = i - 1){
-resultado = resultado + word[i];
-}
+// let word = 'tryber';
+// let resultado = '';
+// let i = word.length;
+// for(let i = word.length - 1 ; 0 <= i ; i = i - 1){
+//     resultado = resultado + word[i];
+// }
+// console.log(resultado);
 
+
+//Exercicio 3 - Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+
+ let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let resultado = 0;
+
+for(let i = 0 ; i <= array.length ; i = i + 1){
+    for(let i2 = 0 ; i2 <= array.length ; i2 = i2 + 1 ){
+resultado = resultado + array [i] [i2];
 console.log(resultado);
 
 
+    }
+}
+//console.log(array[0][0]); 
