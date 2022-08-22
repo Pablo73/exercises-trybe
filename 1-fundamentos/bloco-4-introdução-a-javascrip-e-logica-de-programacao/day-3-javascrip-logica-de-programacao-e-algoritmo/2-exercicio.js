b9f864198aa1ae045abc4b08898209650b6b8f3f
Pablo Dominguez <pasaxo@gmail.com>
2022-08-22 15:52:54 -0300

    finalizado exercicio 3

--- 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-3-javascrip-logica-de-programacao-e-algoritmo/2-exercicio.js
+++ 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-3-javascrip-logica-de-programacao-e-algoritmo/2-exercicio.js
@@ -12,7 +12,7 @@
 
 // let word = 'tryber';
 // let resultado = '';
-// let i = word.length;
+
 // for(let i = word.length - 1 ; 0 <= i ; i = i - 1){
 //     resultado = resultado + word[i];
 // }
@@ -21,16 +21,44 @@
 
 //Exercicio 3 - Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
- let array = ['java', 'javascript', 'python', 'html', 'css'];
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = [0];
+let menor = array[0];
 
-let resultado = 0;
+for(let i = 0 ; i < array.length ; i = i + 1){
 
-for(let i = 0 ; i <= array.length ; i = i + 1){
-    for(let i2 = 0 ; i2 <= array.length ; i2 = i2 + 1 ){
-resultado = resultado + array [i] [i2];
-console.log(resultado);
-
-
-    }
+    if(array[i].length > maior.length){
+     maior = array[i];
+    } 
 }
-//console.log(array[0][0]); 
+
+for(let i = 0 ; i < array.length ; i = i + 1){
+
+    if(array[i].length < menor.length){
+     menor = array[i];
+    } 
+}
+console.log(maior);
+console.log(menor);
+
+
+
+
+// Exrcicio 4 - Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50.
+
+// let maximo = 20;
+// let maiorPrimo = []
+// let primo;
+
+// for(let i = 2 ; i <= maximo ; i = i + 1){
+
+//     for( let i2 = 2 ; i2 <= maximo ; i2 = i2 + 1){
+//         primo = i % i2;  
+//         if(primo === 0){
+//             maiorPrimo.push(i)
+//         }     
+//     }
+// }
+// console.log(maiorPrimo);
+
+  
---
+++ 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-3-javascrip-logica-de-programacao-e-algoritmo/3-bonus.js
@@ -0,0 +1,24 @@
+// Exercicio 1 - Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+
+
+
+// Exercicio 2 - Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+
+
+// Exercicio 3 - Agora inverta o lado do triângulo
+
+
+
+// Exercicio 4 - Depois, faça uma pirâmide com n asteriscos de base
+
+
+
+// Exercicio 5 - Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar
+
+
+
+// Exercicio 6 - Faça um programa que diz se um número definido numa variável é primo ou não.
+//Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
+
+
