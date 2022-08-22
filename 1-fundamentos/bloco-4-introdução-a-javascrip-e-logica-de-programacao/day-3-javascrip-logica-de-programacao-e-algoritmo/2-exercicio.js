4ec0fab61dd9e013785b3671b604ed7e678aa4c6
Pablo Dominguez <pasaxo@gmail.com>
2022-08-22 19:49:10 -0300

    exercicio3

--- 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-3-javascrip-logica-de-programacao-e-algoritmo/2-exercicio.js
+++ 1-fundamentos/bloco-4-introdução-a-javascrip-e-logica-de-programacao/day-3-javascrip-logica-de-programacao-e-algoritmo/2-exercicio.js
@@ -21,44 +21,43 @@
 
 //Exercicio 3 - Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let maior = [0];
-let menor = array[0];
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maior = [0];
+// let menor = array[0];
 
-for(let i = 0 ; i < array.length ; i = i + 1){
+// for(let i = 0 ; i < array.length ; i = i + 1){
 
-    if(array[i].length > maior.length){
-     maior = array[i];
-    } 
-}
+//     if(array[i].length > maior.length){
+//      maior = array[i];
+//     } 
+// }
 
-for(let i = 0 ; i < array.length ; i = i + 1){
+// for(let i = 0 ; i < array.length ; i = i + 1){
 
-    if(array[i].length < menor.length){
-     menor = array[i];
-    } 
-}
-console.log(maior);
-console.log(menor);
+//     if(array[i].length < menor.length){
+//      menor = array[i];
+//     } 
+// }
+// console.log(maior);
+// console.log(menor);
 
 
 
 
 // Exrcicio 4 - Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50.
 
-// let maximo = 20;
-// let maiorPrimo = []
-// let primo;
+let biggestPrimeNumber = 0;
 
-// for(let i = 2 ; i <= maximo ; i = i + 1){
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
 
-//     for( let i2 = 2 ; i2 <= maximo ; i2 = i2 + 1){
-//         primo = i % i2;  
-//         if(primo === 0){
-//             maiorPrimo.push(i)
-//         }     
-//     }
-// }
-// console.log(maiorPrimo);
-
-  
+console.log(biggestPrimeNumber);
