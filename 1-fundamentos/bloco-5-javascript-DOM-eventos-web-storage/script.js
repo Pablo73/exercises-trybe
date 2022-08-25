0d6556e3fdf7768d9b67e349574a475639d0cffc
Pablo Dominguez <pasaxo@gmail.com>
2022-08-25 16:19:47 -0300

    5.1

---
+++ 1-fundamentos/bloco-5-javascript-DOM-eventos-web-storage/script.js
@@ -0,0 +1,10 @@
+
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+//1 -Recupere o elemento que contém o título da página e faça algo com ele, 
+//como alterá-lo para o nome do seu filme favorito.
+
+//document.getElementById('page-title').innerText = "Clube da Luta";
+
+
