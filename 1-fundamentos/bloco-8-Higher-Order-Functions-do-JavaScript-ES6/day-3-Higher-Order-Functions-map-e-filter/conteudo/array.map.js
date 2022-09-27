cee39519e0ba9ce6da198d4cd4539ead7abce356
Pablo Dominguez <pasaxo@gmail.com>
2022-09-27 15:29:50 -0300

    exercicios day3

---
+++ 1-fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/day-3-Higher-Order-Functions-map-e-filter/conteudo/array.map.js
@@ -0,0 +1,187 @@
+// Veja a diferença entre fazer uma mesma função usando for e em seguida usando map:
+
+const persons = [
+    { firstName: 'Maria', lastName: 'Ferreira' },
+    { firstName: 'João', lastName: 'Silva' },
+    { firstName: 'Antonio', lastName: 'Cabral' },
+  ];
+  
+  const fullNames = [];
+  
+  for (let index = 0; index < persons.length; index += 1) {
+    fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+  }
+  
+  console.log(fullNames);
+
+  // -----------------------------------------------------------------------------------
+
+  // Agora com a função map:
+
+  const persons = [
+    { firstName: 'Maria', lastName: 'Ferreira' },
+    { firstName: 'João', lastName: 'Silva' },
+    { firstName: 'Antonio', lastName: 'Cabral' },
+  ];
+  
+  const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+  
+  console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+  
+  // ---------------------------------------------------------------------------------------
+
+//   Vamos praticar um outro exemplo numérico, para fixar bem o que o map faz. Suponha que é preciso transformar todos os números em negativos e passá-los para um array novo.
+
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+
+// E como seria apenas com for?
+
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = [];
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > 0) {
+    negativeNumbers.push(numbers[index] * -1);
+  } else {
+    negativeNumbers.push(numbers[index]);
+  }
+}
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+
+// Outras formas de usar o .map é unir dois arrays para criar um novo. Considere que você possui duas listas: o preço do primeiro 
+// produto (Arroz) é o primeiro elemento da lista prices (2.99), e assim por diante:
+
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
+// Deseja-se juntá-las em apenas uma lista de objetos que fique dessa forma:
+
+const listProducts = [{ Arroz: 2.99 },...]
+
+// Tente criar uma função que resolva esse problema. Funções passadas para as HOFs podem receber 
+// vários parâmetros, não só o elemento sobre o qual ela está sendo iterada! Use isso em seu favor. Caso tenha dúvidas, olhe a solução logo abaixo.
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
+
+const estudantes = [
+    {
+      nome: 'Jorge',
+      sobrenome: 'Silva',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 67 },
+        { name: 'Português', nota: 79 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 65 },
+      ],
+    },
+    {
+      nome: 'Mario',
+      sobrenome: 'Ferreira',
+      idade: 15,
+      turno: 'Tarde',
+      materias: [
+        { name: 'Matemática', nota: '59' },
+        { name: 'Português', nota: '80' },
+        { name: 'Química', nota: '78' },
+        { name: 'Biologia', nota: '92' },
+      ],
+    },
+    {
+      nome: 'Jorge',
+      sobrenome: 'Santos',
+      idade: 15,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '76' },
+        { name: 'Português', nota: '90' },
+        { name: 'Química', nota: '70' },
+        { name: 'Biologia', nota: '80' },
+      ],
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Silveira',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '91' },
+        { name: 'Português', nota: '85' },
+        { name: 'Química', nota: '92' },
+        { name: 'Biologia', nota: '90' },
+      ],
+    },
+    {
+      nome: 'Natalia',
+      sobrenome: 'Castro',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '70' },
+        { name: 'Português', nota: '70' },
+        { name: 'Química', nota: '60' },
+        { name: 'Biologia', nota: '50' },
+      ],
+    },
+    {
+      nome: 'Wilson',
+      sobrenome: 'Martins',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '80' },
+        { name: 'Português', nota: '82' },
+        { name: 'Química', nota: '79' },
+        { name: 'Biologia', nota: '75' },
+      ],
+    },
+  ];
+
+  // -------------------------------------------------------------------
+
+// for
+const allNameStudents = [];
+
+for (let index = 0; index < estudantes.length; index += 1 ) {
+  if(estudantes[index].turno === 'Manhã') {
+    allNameStudents.push(`${estudantes[index].nome} ${estudantes[index].sobrenome}`)
+  }
+}
+
+console.log(allNameStudents);
+
+// Com map e filter.
+
+const allNameStudents = estudantes.filter((estudante) => (
+    estudante.turno === 'Manhã')).map((estudante) => `${estudante.nome} ${estudante.sobrenome}`);
+  
+  console.log(allNameStudents);
+  
+
+
