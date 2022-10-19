2f6b2d4f14322ba3417a2a1e8b906ea8a88e2551
Pablo Dominguez <pasaxo@gmail.com>
2022-10-19 15:13:15 -0300

    Exercicios de fixação

---
+++ 2-front-end/secao-2-introducao-react/day-1-hello-world-no-react/exrcicio/my-app/src/About.js
@@ -0,0 +1,18 @@
+function About1() {
+    return (
+    <heder>
+      <h1>Pablo</h1>
+        <p> salame, salame, salame, salame, salame, salame, salame, salame, salame, </p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Higher orden function</li>
+        </ul>
+    </heder>
+    )  
+}
+
+export default About1;
