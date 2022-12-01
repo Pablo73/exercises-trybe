694a208e1bddb86745d134dfe6081b1a7b568c72
Pablo Dominguez <pasaxo@gmail.com>
2022-12-01 08:03:01 -0300

    finalização do conteudo

---
+++ 2-front-end/secao-7-gerenciamento-de estado-global/day-2-redux-react/conteudo-do-dia/my-app/src/redux/index.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
+
