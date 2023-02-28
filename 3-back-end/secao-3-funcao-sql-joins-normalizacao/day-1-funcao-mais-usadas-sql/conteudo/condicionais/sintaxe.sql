27c98520d2df39a006f814f14f1cfe91d892bd22
Pablo Dominguez <pasaxo@gmail.com>
2023-02-28 14:28:21 -0300

    condicional

---
+++ 3-back-end/secao-3-funcao-sql-joins-normalizacao/day-1-funcao-mais-usadas-sql/conteudo/condicionais/sintaxe.sql
@@ -0,0 +1,43 @@
+-- Sintaxe:
+SELECT IF(condicao, valor_se_verdadeiro, valor_se_falso);
+
+SELECT IF(idade >= 18, 'Maior de idade', 'Menor de Idade')
+FROM pessoas;
+
+SELECT IF(aberto, 'Entrada permitida', 'Entrada não permitida')
+FROM estabelecimentos;
+
+-- Exemplo utilizando o banco sakila:
+SELECT first_name, IF(active, 'Cliente Ativo', 'Cliente Inativo') AS status
+FROM sakila.customer
+LIMIT 20;
+
+-- Sintaxe:
+SELECT CASE
+  WHEN condicao THEN valor
+  ELSE valor padrao
+END;
+
+SELECT
+    nome,
+    nivel_acesso,
+    CASE
+        WHEN nivel_acesso = 1 THEN 'Nível de acesso 1'
+        WHEN nivel_acesso = 2 THEN 'Nível de acesso 2'
+        WHEN nivel_acesso = 3 THEN 'Nível de acesso 3'
+        ELSE 'Usuário sem acesso'
+    END AS nivel_acesso
+FROM permissoes_usuario;
+
+-- Exemplo utilizando a tabela sakila.film:
+SELECT
+    first_name,
+    email,
+    CASE
+        WHEN email = 'MARY.SMITH@sakilacustomer.org' THEN 'Cliente de baixo valor'
+        WHEN email = 'PATRICIA.JOHNSON@sakilacustomer.org' THEN 'Cliente de médio valor'
+        WHEN email = 'LINDA.WILLIAMS@sakilacustomer.org' THEN 'Cliente de alto valor'
+        ELSE 'não classificado'
+    END AS valor
+FROM sakila.customer
+LIMIT 10;
