c641d618445197d4fa53059fee505c79728eeadc
Pablo Dominguez <pasaxo@gmail.com>
2023-05-26 11:40:12 -0300

    exercicios do conteudo

---
+++ 4-ciencia-da-computacao/secao-1-introducao-python/day-1-aprendendo-python/conteudo/material-conteudo/1-opracoes-basicas.py
@@ -0,0 +1,16 @@
+2 * 3 
+
+square_root = 25 ** (1/2) # raiz quadrada 
+print(square_root + 1)
+
+# original
+counter = counter + 1
+
+# simplificado
+counter += 1
+
+3 // 2  # saída: 1
+
+3 / 2  # saída: 1.5
+
+# Note que o operador // realiza a divisão e arredonda o resultado para baixo. Ou seja, realiza o quociente.
